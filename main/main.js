00e94368a77a71999542b5ac6544bfcbf2143f43
brobird <netassadove@gmail.com>
2017-06-24 21:57:33 +0800

    initial main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,61 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+    var input = number.toString();
+    var table = [
+      ["._.","|.|","|_|"],
+      ["...","..|","..|"],
+      ["._.","._|","|_."],
+      ["._.","._|","._|"],
+      ["...","|_|","..|"],
+      ["._.","|_.","._|"],
+      ["._.","|_.","|_|"],
+      ["._.","..|","..|"],
+      ["._.","|_|","|_|"],
+      ["._.","|_|","..|"]
+    ]
+    var txt = "";
+    var flag = 0;
+    var j = 0;
+    while(flag!=input.length * 3) {
+      for(var i=0;i<input.length;i++) {
+        switch (input[i]) {
+          case "0":
+            txt += table[0][j] + " ";
+            break;
+          case "1":
+            txt += table[1][j] + " ";
+            break;
+          case "2":
+            txt += table[2][j] + " ";
+            break;
+          case "3":
+            txt += table[3][j] + " ";
+            break;
+          case "4":
+            txt += table[4][j] + " ";
+            break;
+          case "5":
+            txt += table[5][j] + " ";
+            break;
+          case "6":
+            txt += table[6][j] + " ";
+            break;
+          case "7":
+            txt += table[7][j] + " ";
+            break;
+          case "8":
+            txt += table[8][j] + " ";
+            break;
+          case "9":
+            txt += table[9][j] + " ";
+            break;
+          default:
+        }
+        flag++;
+    }
+    txt = txt.substring(0, txt.length-1);
+    txt += "\n";
+    j++;
+    }
+    console.log(txt);
+    return txt;
 };
